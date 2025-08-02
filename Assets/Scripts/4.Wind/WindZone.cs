6d91e46963bccebdfb0f68963dd17ae588f70191
wndwp <wndwpduswn@naver.com>
2025-08-02 19:39:04 +0900

    바람 랜덤 수정 주석 처리

--- Assets/Scripts/4.Wind/WindZone.cs
+++ Assets/Scripts/4.Wind/WindZone.cs
@@ -4,8 +4,8 @@ using UnityEngine;
 
 public class WindZone : MonoBehaviour
 {
-    public Vector3 windDirection = Vector3.right; // 바람 방향
-    public float windStrength = 5f; // 힘의 크기
+    public Vector3 windDirection = Vector3.right;
+    public float windStrength = 5f;
 
     private void OnTriggerStay(Collider other)
     {
@@ -19,3 +19,62 @@ public class WindZone : MonoBehaviour
         }
     }
 }
+//using System;
+//using System.Collections;
+//using UnityEngine;
+
+//public class WindZone : MonoBehaviour
+//{
+//    public Vector3 windDirection = Random.onUnitSphere;
+//    public float windStrength = Random.Range(2f, 10f);
+
+//    public Vector3 minPosition = new Vector3(-10, 0, -10);
+//    public Vector3 maxPosition = new Vector3(10, 0, 10);
+//    public Vector3 minSize = new Vector3(3, 3, 3);
+//    public Vector3 maxSize = new Vector3(6, 6, 6);
+
+//    public float changeInterval = 5f;
+
+//    private BoxCollider boxCollider;
+
+//    private void Start()
+//    {
+//        boxCollider = GetComponent<BoxCollider>();
+//    }
+
+//    public void ChangeZoneRandomly() // GameMangager의 Update함수에 조건+함수호출로 사용
+//    {
+//         // 위치 랜덤
+//         transform.position = new Vector3(
+//             Random.Range(minPosition.x, maxPosition.x),
+//             Random.Range(minPosition.y, maxPosition.y),
+//             Random.Range(minPosition.z, maxPosition.z)
+//         );
+
+//         // 크기 랜덤
+//         if (boxCollider != null)
+//         {
+//             boxCollider.size = new Vector3(
+//                 Random.Range(minSize.x, maxSize.x),
+//                 Random.Range(minSize.y, maxSize.y),
+//                 Random.Range(minSize.z, maxSize.z)
+//             );
+//         }
+
+//         windDirection = Random.onUnitSphere;
+//         windStrength = Random.Range(2f, 10f);
+
+//    }
+
+//    private void OnTriggerStay(Collider other)
+//    {
+//        if (other.CompareTag("Projectile"))
+//        {
+//            Rigidbody rb = other.GetComponent<Rigidbody>();
+//            if (rb != null)
+//            {
+//                rb.AddForce(windDirection * windStrength, ForceMode.Force);
+//            }
+//        }
+//    }
+//}
